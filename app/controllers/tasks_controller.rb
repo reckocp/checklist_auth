d22b65d80615432190ad17f3b0d17aafab25d50f
COLIN <reckocp@gmail.com>
2016-04-05 11:39:53 -0500

    more formatting

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
 
 private
 
-def task_params
-params.require(:task).permit(:body, :position, :completed)
-end
+  def task_params
+    params.require(:task).permit(:body, :position, :completed)
+  end
 end
